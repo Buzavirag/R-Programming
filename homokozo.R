ff5eb3f7f981798705c2852bc9ef70d08db2edfd
Buzavirag <bucoka@yahoo.com>
2014-12-12 01:15:49 +0100

    proba

--- homokozo.R
+++ homokozo.R
@@ -1,4 +1,75 @@
-myfunction <- function() {
-x <- rnorm(100)
-mean(x)
+my_if1 <- function() {
+	x <- rnorm(1)
+	if(x > 0) {
+		y <- "nagyobb"
+		print(y)
+	} else {
+	y <- "nem nagyobb"
+	print(y)
+	}
+	print(x)
+}
+
+my_if2 <- function() {
+	x <- rnorm(1)
+	y <- if(x > 0) {
+		"nagyobb"
+	} else {
+		"nem nagyobb"
+	}
+	print(y)
+	print(x)
+}
+
+my_loop1 <- function(){
+	x <- c("a", "b")
+	for(i in 1:2) print(x[i])
+	y <- c("A", "B")
+	for(i in 1:length(y)) {
+		print(y[i])
+	}
+	z <- c("aA", "bB")
+	for(i in seq_along(z)) print(z[i])
+	v <- c("aB", "cD")
+	for(letter in v) print(letter)
+}
+
+my_loop2 <- function() {
+	m <- matrix(1:6, 2, 3)	
+	for(i in seq_len(nrow(m))) for(j in seq_len(ncol(m))) print(m[i, j])
+}
+
+my_loop3 <- function(){
+	m <- matrix(NA, 2, 3)
+	x <- c("a", "b")
+	for(i in 1:2) m[i, 1] <- x[i]
+	y <- c("A", "B")
+	for(i in 1:length(y)) m[i, 2] <- y[i]
+	z <- c("aA", "bB")
+	for(i in seq_along(z)) m[i, 3] <- z[i]
+	print(m)
+}
+
+my_while <- function(x) {
+	m <- NA
+	count <- 1
+	while(count <= x) {
+		m[count] <- count
+		count <- count + 1
+	}
+	print(m)
+	class(m)
+}
+
+my_coin <- function() {
+	z <- 5
+	while(z >= 4 && z <= 7) {
+		print(z)
+		coin <- rbinom(1, 1, 0.5)
+		if(coin == 1) { ## random walk
+			z <- z + 1
+		} else {
+			z <- z - 1
+		}
+	}
 }
